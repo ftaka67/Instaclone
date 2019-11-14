72a10bfde70da97c0b679a4fdc1e60404699a98f
Roux <roux@example.com>
2019-11-14 12:50:01 +0000

    add login form

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   root 'top_pages#home'
   get  '/signup', to: 'users#new'
   resources :users
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
 end
