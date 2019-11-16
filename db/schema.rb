c83319d6d5b872280fea557d4ecc579105cbc9e6
Roux <roux@example.com>
2019-11-16 13:27:48 +0000

    Finish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191115070718) do
+ActiveRecord::Schema.define(version: 20191116131125) do
 
   create_table "users", force: :cascade do |t|
     t.string "fullname"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20191115070718) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(fullname:  "Full Example User",
+             name: "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+
+99.times do |n|
+  fullname  = Faker::Name.name
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(fullname:  fullname,
+               name: name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
