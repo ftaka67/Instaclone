cbfece6ea7aab9c27ee3eed547d8946b1ffbdcc5
Roux <roux@example.com>
2019-11-17 04:46:23 +0000

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
 #  get 'top_pages/home'
   root 'top_pages#home'
   get  '/signup', to: 'users#new'
-  resources :users
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  resources :users
+  resources :account_activations, only: [:edit]
 
 end
