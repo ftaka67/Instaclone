5dcd66ab44a465347e22fa898dfdefb8dbf7c4c6
Roux <roux@example.com>
2019-12-05 17:43:04 +0000

    modify user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -96,7 +96,7 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
       user.fullname = auth.info.name
-      user.authenticated = true
+      user.activated = true
 #      user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails, 
       # uncomment the line below to skip the confirmation emails.
